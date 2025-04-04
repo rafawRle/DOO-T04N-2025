84b444ccd4107779f56e0e96888b19bf9cd685a5
Vinicius Esser Marques <vemarques@minha.fag.edu.br>
2025-04-04 20:22:30 -0300

    Add files via upload

---
+++ Alunos/Vinicius Esser/Aula_06/Vendedor.java
@@ -0,0 +1,38 @@
+package Aula_06;
+
+public class Vendedor {
+    String nome;
+    int idade;
+    String loja;
+    String cidade;
+    String bairro;
+    String rua;
+    double salarioBase;
+    double[] salarioRecebido = {2000.0, 2100.0, 2200.0}; 
+
+    public Vendedor(String nome, int idade, String loja, String cidade, String bairro, String rua, double salarioBase) {
+        this.nome = nome;
+        this.idade = idade;
+        this.loja = loja;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.salarioBase = salarioBase;
+    }
+
+    public void apresentarse() {
+        System.out.println("Nome: " + nome + ", Idade: " + idade + ", Loja: " + loja);
+    }
+
+    public double calcularMedia() {
+        double soma = 0;
+        for (double salario : salarioRecebido) {
+            soma += salario;
+        }
+        return soma / salarioRecebido.length;
+    }
+
+    public double calcularBonus() {
+        return salarioBase * 0.2;
+    }
+}
