197d04503bb453fb850b572c1fe81fd2d72ff41f
Shayckan <arthurmarcb@gmail.com>
2025-04-18 21:18:17 -0300

    Create pedido.java

---
+++ Alunos/Arthur/Aula 09/pedido.java
@@ -0,0 +1,34 @@
+import java.util.Date;
+import java.util.List;
+
+public class Pedido {
+    private int id;
+    private Date dataCriacao;
+    private Date dataPagamento;
+    private Date dataVencimentoReserva;
+    private Cliente cliente;
+    private Vendedor vendedor;
+    private String loja;
+    private List<Item> itens;
+
+    public Pedido(int id, Date dataCriacao, Date dataPagamento, Date dataVencimentoReserva,
+                  Cliente cliente, Vendedor vendedor, String loja, List<Item> itens) {
+        this.id = id;
+        this.dataCriacao = dataCriacao;
+        this.dataPagamento = dataPagamento;
+        this.dataVencimentoReserva = dataVencimentoReserva;
+        this.cliente = cliente;
+        this.vendedor = vendedor;
+        this.loja = loja;
+        this.itens = itens;
+    }
+
+    public double calcularValorTotal() {
+        return itens.stream().mapToDouble(Item::getValor).sum();
+    }
+
+    public void gerarDescricaoVenda() {
+        System.out.println("Pedido criado em: " + dataCriacao);
+        System.out.println("Valor total: R$" + calcularValorTotal());
+    }
+}
