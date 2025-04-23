9ac66020eefc9a27dcf09e962f5aeac677059a78
Vinicius Esser Marques <vemarques@minha.fag.edu.br>
2025-04-22 21:29:28 -0300

    Add files via upload

---
+++ Alunos/Vinicius Esser/Livro.java
@@ -0,0 +1,32 @@
+package BibliotecaPub;
+
+public abstract class Livro {
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;  
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public abstract boolean podeSerEmprestado();  
+    public abstract String getDescricao();  
+}
